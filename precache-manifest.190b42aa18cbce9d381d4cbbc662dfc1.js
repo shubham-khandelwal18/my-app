61e2ecc116b93791d5244fb68431af8203794786
Shubham Khandelwal <shubhamsharma18082001@gmail.com>
2020-07-06 10:54:59 +0530

    Updates

---
+++ precache-manifest.190b42aa18cbce9d381d4cbbc662dfc1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8e93f5f26f11ac67fed050537e4359b",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "8494e3540bf39793607f",
+    "url": "/my-app/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "e3945a0f13a8197696ae",
+    "url": "/my-app/static/css/main.021a97be.chunk.css"
+  },
+  {
+    "revision": "8494e3540bf39793607f",
+    "url": "/my-app/static/js/2.8931b4a4.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/my-app/static/js/2.8931b4a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3945a0f13a8197696ae",
+    "url": "/my-app/static/js/main.9f7ddd2e.chunk.js"
+  },
+  {
+    "revision": "aba70bfb0e638b9bd36d",
+    "url": "/my-app/static/js/runtime-main.3bebbd9a.js"
+  },
+  {
+    "revision": "40c428b3149b445ad87ccdda5316c436",
+    "url": "/my-app/static/media/codechef.40c428b3.jpg"
+  },
+  {
+    "revision": "1db763f25813c73281498833ff4c189a",
+    "url": "/my-app/static/media/codeforces.1db763f2.png"
+  }
+]);
